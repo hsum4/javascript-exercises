50b4b805dd65d8349f58268ea026dbc1980975cc
hsum4 <93267846+hsum4@users.noreply.github.com>
2024-11-17 21:05:06 +0800

    finish reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const message = string.split("");
+    let reversedMessage = "";
+    for (i = message.length - 1; i >= 0; i--){
+        reversedMessage += message[i];
+    }
+    return reversedMessage;
 };
 
 // Do not edit below this line
