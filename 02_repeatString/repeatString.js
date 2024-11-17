7a8a809d2d10e1ceb0b28ebb3b5768614c6949e8
hsum4 <93267846+hsum4@users.noreply.github.com>
2024-11-17 20:39:10 +0800

    finish helloWorld and repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let message = "";
+    if (number < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < number; i++){
+        message += string;
+    }
+    return message;
 };
 
 // Do not edit below this line
