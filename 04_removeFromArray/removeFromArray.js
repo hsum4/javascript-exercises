900dfd25fde5c7ae99c172198a84c204c62f0e6d
hsum4 <93267846+hsum4@users.noreply.github.com>
2024-11-17 22:04:51 +0800

    finish removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toBeRemoved) {
+    for (let i = 0; i < toBeRemoved.length; i++){
+        let value = toBeRemoved[i];
+        while (array.indexOf(value) !== -1){
+            array.splice(array.indexOf(value), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
