0f6728150a2d2e7792ffd42741bda462b2653367
hsum4 <93267846+hsum4@users.noreply.github.com>
2024-11-18 11:09:44 +0800

    finish sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    let finalSum = 0;
+    if (firstNum < 0 || secondNum < 0 || !Number.isInteger(firstNum) || !Number.isInteger(secondNum)){
+        return "ERROR";
+    }
+    else if (firstNum < secondNum){
+        for (i = firstNum; i <= secondNum; i++){
+            finalSum += i;
+        }
+    }
+    else {
+        for (i = secondNum; i <= firstNum; i++){
+            finalSum += i;
+        }
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
