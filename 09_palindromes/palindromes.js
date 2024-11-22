15f0573c4803c9fee726268d4186f21c7972cf51
hsum4 <93267846+hsum4@users.noreply.github.com>
2024-11-22 13:31:58 +0800

    finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+const charsToBeRemoved = ["!", " ", ",", "."];
+let cleanedString = "";
 
+for (let char of str){
+    if(!charsToBeRemoved.includes(char)){
+        cleanedString += char.toLowerCase();
+    }
+}
+
+reversedStr = cleanedString.split("").reverse().join("");
+
+return cleanedString === reversedStr ? true : false;
 };
 
 // Do not edit below this line
