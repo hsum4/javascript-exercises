97779f3884b2c579e24c86f791966810ab74fb6a
hsum4 <93267846+hsum4@users.noreply.github.com>
2024-11-22 14:03:08 +0800

    finish fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+if (typeof num !== 'number'){
+    num = Number(num);
+}
+
+if (num === 0) return 0;
+if (num < 0) return "OOPS";
+
+let result = 1;
+let prevNum1 = 1;
+let prevNum2 = 1;
+
+for(i = 2 ; i < num; i++){
+    result = prevNum1 + prevNum2;
+    prevNum2 = prevNum1;
+    prevNum1 = result;
+}
+
+return result;
 };
 
 // Do not edit below this line
