c9ae54da4d56e55a288def1dc2b774fb62e9e498
hsum4 <93267846+hsum4@users.noreply.github.com>
2024-11-22 12:48:37 +0800

    finish multiply function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,8 +10,8 @@ const sum = function(arr) {
   return arr.reduce((total, current) => (total += current), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current) => (total *= current));
 };
 
 const power = function() {
