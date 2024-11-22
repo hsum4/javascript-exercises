74f9313f1b4992762b518008f37b99e60e000b67
hsum4 <93267846+hsum4@users.noreply.github.com>
2024-11-22 14:34:33 +0800

    finish find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const oldestPerson = arr.sort((a,b) => {
+    let personA = (a.yearOfDeath || 2024) - (a.yearOfBirth || 2024);
+    let personB = (b.yearOfDeath || 2024) - (b.yearOfBirth || 2024);
+    return personB - personA;
+    })
 
+    return oldestPerson[0];
 };
 
 // Do not edit below this line
